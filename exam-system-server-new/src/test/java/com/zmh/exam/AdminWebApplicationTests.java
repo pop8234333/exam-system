d06df8517dfe8610920c9469a4b1bb9583e6b783
wulilemon <2059901048@qq.com>
2025-11-24 11:50:40 +0800

    后端大致完成代码

---
+++ exam-system-server-new/src/test/java/com/zmh/exam/AdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zmh.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
