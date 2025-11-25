5e4ff19bb4bd9842f8d16f890796a0bf81e8bb33
wulilemon <2059901048@qq.com>
2025-11-25 16:02:57 +0800

    全局统一异常处理

---
+++ exam-system-server-new/src/main/java/com/zmh/exam/common/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.zmh.exam.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author zmh
+ * @since 2025/11/25 15:43<br/>
+ * -----------------------------<br/>
+ * Project    : exam-system<br/>
+ * Package    : com.zmh.exam.common<br/>
+ * ClassName  : GlobalExceptionHandler<br/>
+ * Description:  请填写类的描述
+ */
+@Slf4j
+@RestControllerAdvice//统一异常处理
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public Result<Exception> handleException(Exception e) {
+        log.error("服务器发生运行时异常！", e);
+        //记录异常日志
+        log.error("服务器发生运行时异常！异常信息为：{}",e.getMessage());
+        return Result.error("服务器发生运行时异常！");
+    }
+
+}
